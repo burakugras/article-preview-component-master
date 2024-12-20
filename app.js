df20780ea81bc26bf7d6d6dfabd4f08d6a72fa97
burakugras <burakugras67@gmail.com>
2024-12-20 21:38:27 +0300

    added js file & refactoring

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const shareButton = document.getElementById('share-icon')
 
-shareButton.addEventListener('onclick',()=>{
-    
+shareButton.addEventListener('click', () => {
+    const popup = document.getElementById('popup')
+    popup.classList.toggle('hidden');
 })
